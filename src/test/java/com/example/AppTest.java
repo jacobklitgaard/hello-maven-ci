61712d6dd17f2cfe8049fdfbdee929203f16f545
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2026-02-12 20:48:19 +0100

    Initial Maven project with tests

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+
+    @Test
+    public void testAddNegative() {
+        assertEquals(-1, App.add(2, -3));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, App.multiply(2, 3));
+    }
+}
