b76bf2f2c4ec4bb42f518894b003a74ec0a85c68
Jacob Klitgaard <jacobklitgaardhansen@gmail.com>
2026-06-05 16:13:36 +0200

    a change

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Maven!");
         System.out.println("2 + 3 = " + add(2, 3));
+        System.out.println("a change");
     }
 
     public static int add(int a, int b) {
@@ -13,5 +16,6 @@ public class App {
     public static int multiply(int a, int b) {
         return a * b;
     }
+
 }
 // Updated
